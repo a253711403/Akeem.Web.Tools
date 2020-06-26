55fca29647ea77500d644d7be475a689552b37b1
赛 姚 <253711403@qq.com>
2020-06-26 22:47:31 +0800

    项目初始化

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Akeem.Web.Tools.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
